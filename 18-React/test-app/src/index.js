4c7673b0eb3c12d12833cb872e0003ff7a4c70fe
Nitesh Sharma <nitesh.grep@gmail.com>
2018-06-09 09:22:59 +0530

    react

--- 18-React/test-app/src/index.js
+++ 18-React/test-app/src/index.js
@@ -17,32 +17,74 @@ function BoilingVerdict(props){
     }
 }
 
+
+
+
+class Temprature extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+
+        //if fehr
+
+    }
+
+    
+    handleChange(event){
+        this.props.updateVerdict(event.target.value);
+    }
+
+    render(){
+        return(<div>
+            <input type="text" value={this.props.temprature} onChange={this.handleChange}/>
+        </div>
+        )
+    }
+}
+
+
 class Calculator extends React.Component{
 
     constructor(props){
         super(props);
         this.state={
-            temprature: 0
+            temprature: 0,
+            scale: "c"
         }
         
         this.updateVerdict = this.updateVerdict.bind(this);
     }
 
-    updateVerdict(event){
+    updateVerdict(value){
     
         this.setState({
-            temprature: event.target.value
+            temprature: value
         })
     }
 
     render(){
+
+        const scale = this.state.scale
+        const temprature = this.state.temprature
+        let celcius = temprature
+        let fehrenhiet = temprature;
+
+        if(scale === "c"){
+            fehrenhiet = parseInt(temprature) + 100;
+        }
+
+        if(scale === "c"){
+            celcius = parseInt(temprature) + 200;
+        }
         return(
-            <div>
-                <input type="text" onChange={this.updateVerdict}/>
+            <div>Celsius
+                <Temprature updateVerdict={this.updateVerdict} temprature = {celcius} scale="c"/> 
+                <Temprature  updateVerdict={this.updateVerdict} temprature = {fehrenhiet} scale="f"/> 
                 <BoilingVerdict celsius= {this.state.temprature}/>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Calculator celsius={200} />, document.getElementById('root'))
+ReactDOM.render(<Calculator sclae="" celsius={200} />, document.getElementById('root'))
